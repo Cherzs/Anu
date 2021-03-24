70c28f1b6fcc3283ee128c6ed66445171f536e79
Cherzs <zhafrang638@gmail.com>
2021-03-24 15:37:58 +0700

    login

---
+++ app/src/main/java/com/example/cherzs/MainActivity2.kt
@@ -0,0 +1,22 @@
+package com.example.cherzs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+        login.setOnClickListener{
+           val intent: Intent()
+            intent = intent()
+    }
+
+
+}
